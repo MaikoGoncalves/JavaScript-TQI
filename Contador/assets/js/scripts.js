dcaeb055a444e47febdf8e010d62ff1b64116268
Maiko Soares Gonçalves <64238160+MaikoGoncalves@users.noreply.github.com>
2022-06-23 00:29:24 -0300

    23062022

--- Contador/assets/js/scripts.js
+++ Contador/assets/js/scripts.js
@@ -7,12 +7,16 @@ function trocarCor(){
 
     if (contador < 0) { 
         document.getElementById("currentNumber").style.color = "red";
+        document.getElementById("h1").style.color = "red";
     }   
         else if (contador > 0) {
             document.getElementById("currentNumber").style.color = "black";
+            document.getElementById("h1").style.color = "black";
         }   
-             else 
+             else {
                 document.getElementById("currentNumber").style.color = "white";
+                document.getElementById("h1").style.color = "white";
+             }
 }
 
 
