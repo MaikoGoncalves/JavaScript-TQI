01c759b3ced4e946193ee9af6262cdb47937a7fe
Maiko Soares Gonçalves <64238160+MaikoGoncalves@users.noreply.github.com>
2022-07-12 13:06:17 -0300

    12072022

--- Palindromo/script.js
+++ Palindromo/script.js
@@ -9,12 +9,31 @@ function limpaPalavra(palavra){
     palavra = palavra.replace(/[ ,-]+/gi, "");
     return palavra;
 }
-function ehPalindromo(palavra, palavraReal){
+
+function ehPalavraFrase (palavraFrase, palavraNova){
+    var palavraNova = document.getElementById("frase").value;
+    let palavraT = Array.from(palavraNova);
+    var palavraFrase = 'Palavra';
+    
+    for(let i = 0 ;  i < palavraT.length ; i++)
+    {
+         if (palavraT[i] === " ")
+         {
+            palavraFrase = "Frase";
+         } 
+    }
+    return  palavraFrase;
+
+}
+function ehPalindromo(palavra, palavraReal, palavraFrase){
     var palavraReal = document.getElementById("frase").value;
+    
+    palavraFrase = ehPalavraFrase(palavraFrase);
+    
     palavra = limpaPalavra(palavra);
     var palavraInvertida = palavra.split("").reverse().join("");
     if(palavraInvertida == palavra){
-        return res.innerHTML = 'A Palavra ou Frase: " ' + palavraReal + ' " é Palindromo'  
+        return res.innerHTML = 'A '+ palavraFrase + ' : " ' + palavraReal + ' " é Palindromo'  
     }
-    return res.innerHTML = 'A Palavra ou Frase: " ' + palavraReal + ' " Não é Palindromo' 
+    return res.innerHTML = 'A '+ palavraFrase + ' : " ' + palavraReal + ' " Não é Palindromo' 
 }
