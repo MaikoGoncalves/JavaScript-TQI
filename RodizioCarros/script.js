f4b6c360fe055576a45247ae2bab90d6cca1ece1
Maiko Soares Gonçalves <64238160+MaikoGoncalves@users.noreply.github.com>
2022-08-15 21:57:30 -0300

    15-08-2022

--- RodizioCarros/script.js
+++ RodizioCarros/script.js
@@ -1,5 +1,5 @@
 function placa(){  
-
+    //
     const regexPlacaValida = new RegExp("^[A-Z]{3}-[0-9]{4}$");
     const placaDigitado = document.getElementById("placa").value;
     const placa = placaDigitado;
@@ -11,28 +11,23 @@ function placa(){
                 return  res.innerHTML = "SEGUNDA"
                 case 3:
                 case 4:   
-                return  res.innerHTML = "TERCA"
-                
+                return  res.innerHTML = "TERCA"                
                 case 5:
                 case 6:   
-                return  res.innerHTML = "QUARTA"
-               
+                return  res.innerHTML = "QUARTA"               
                 case 7:
                 case 8:   
                 return  res.innerHTML  = "QUINTA"
                 case 9:
                 case 0:   
                 return  res.innerHTML = "SEXTA"
-               
-            }
+             }
            // return  res.innerHTML = ((("FINAL DA PLACA ") + placa[7]) + digito); 
           // TODO: Recupere o digito final da placa e o atribua à constante "digito".
           // TODO: A partir do "digito", crie as condições necessárias para imprimir o dia da semana.
         } else {
             return  res.innerHTML = ("Placa não valida");
-        }
-      
-          
+        }          
 }
 
 
